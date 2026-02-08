9369fd43f5d199bd062ff4d4b16f293af36bd857
Ranica10 <ranicachawla101@gmail.com>
2026-02-08 12:00:42 -0500

    small fix

--- street_wise/street-wise/src/Main.vue
+++ street_wise/street-wise/src/Main.vue
@@ -104,7 +104,7 @@
           </div>
         </div>
 
-        <div class="reasonSummary">
+        <div class="reasonSummary" v-if="!loading && !reasoningText">
           {{ shortReason || "No reasoning yet. Enter start/end and click Find Best Route." }}
         </div>
 
